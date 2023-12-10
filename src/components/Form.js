d81005a88e8436722628b3b791dfe7e3bd8202aa
amirah <mirez.data@gmail.com>
2023-12-10 23:09:08 +0800

    commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,16 +2,18 @@ import React from "react";
 
 function form(){
     return(
-        <div className="body">
-            <div className="form">
+        <div className="body-form">
+            <div className="form-top">
             <p>Top text</p>
-            <input type="text" id="inputField" placeholder="Enter top text" />
-            
+            <input type="text" id="inputField" placeholder="Enter text" className="input-text"/>
+            </div>
+
+            <div className="form-bottom">
             <p>Bottom text</p>
-            <input type="text" id="inputField" placeholder="Enter bottom text" />
+            <input type="text" id="inputField" placeholder="Enter text" className="input-text" />
             </div>
             
-            <button type="button" id="submit" className="button">Get new meme image</button>
+            <button type="button" id="submit" className="button-form">Get new meme image</button>
 
         </div>
     )
--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header(){
     return(
         <div className="header">
-            <img src="./images/bird-logo.png" alt="meme-logo" className="logo" />
+            <img src="../images/bird-logo.png" alt="logo" className="logo" />
             <h2 className="title">Meme Generator</h2>
             <h4 className="project-name">React Course - Project 3</h4>
         </div>
