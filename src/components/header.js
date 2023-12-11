cdba78d53a86f6e9418cfc18003c5dadd7288292
amirah <mirez.data@gmail.com>
2023-12-11 23:47:07 +0800

    commit

--- src/components/header.js
+++ src/components/header.js
@@ -3,8 +3,8 @@ import React from "react";
 function Header(){
     return(
         <div className="header">
-            <img src="../images/bird-logo.png" alt="logo" className="logo" />
-            <h2 className="title">Meme Generator</h2>
+            <img src={process.env.PUBLIC_URL + '/image/bird-logo.png'} alt="logo" className="logo" />
+            <h2 className="title">MemeGenerator</h2>
             <h4 className="project-name">React Course - Project 3</h4>
         </div>
     )
