a20f3df95d8483877d947c28d03a605ab77d5bc9
amirah <mirez.data@gmail.com>
2023-12-18 15:23:07 +0800

    commit

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,17 +2,17 @@ import React from "react";
 import memesData from "./memeData";
 
 function Meme(){
+    const [memeImage, setMemeImage] = React.useState("")
     
     function getMemeImage(imageArr){
         const memeArray = memesData.data.memes
         const randomIndex = Math.floor(Math.random() * memeArray.length);
-        const memeUrl= memeArray[randomIndex].url 
 
-        return memeUrl;
+        setMemeImage( memeImage => (
+            memeImage = memeArray[randomIndex].url
+        ))
         }
 
-
-    
     return(
         <main className="form-content">
             <div className="form-body">
@@ -28,8 +28,9 @@ function Meme(){
                 placeholder="Bottom text" 
                 className="input-text"/>
             
-            <button type="button" id="submit" className="button-form">Get a new meme image</button>
+            <button type="button" id="submit" className="button-form" onClick={getMemeImage}>Get a new meme image</button>
             </div>
+            <img src={memeImage} className="meme-image"/>
         </main>
     )
 }
